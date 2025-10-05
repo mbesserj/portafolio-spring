788786a67957daed7dabf674fbbfcc657ff54ebb
mbesserj <64606876+mbesserj@users.noreply.github.com>
2025-10-05 02:23:03 -0300

    Delete portafolio-app directory

--- portafolio-app/src/main/java/com/portafolio/app/services/EmpresaService.java
+++
@@ -1,9 +0,0 @@
-package com.portafolio.app.services;
-
-import com.portafolio.model.dto.EmpresaDto;
-import java.util.List;
-
-public interface EmpresaService {
-    List<EmpresaDto> findAll();
-    EmpresaDto save(EmpresaDto empresaDTO);
-}
